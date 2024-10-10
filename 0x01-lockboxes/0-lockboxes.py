9bc311598a0429779a7676da1fc62f84a4dd77d1
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-10 15:44:29 +0100

    sloving lock boxes problem

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,24 +4,48 @@
 
 def canUnlockAll(boxes):
     """
-    Solves the lockboxes problem using Depth-First Search with recursion.
-    Time Complexity: O(N + K)
-    Space Complexity: O(N) - Due to recursion stack
+    Determines if all boxes in a list of locked boxes can be opened.
+
     Args:
-        boxes: List of lists containing keys to other boxes
+        boxes (list of lists): A list of lists where
+                              each inner list contains keys.
+                              Keys are positive integers.
+                              A key with the same number
+                              as a box opens that box.
+                              The first box boxes[0] is unlocked.
+                              There can be keys that do not have boxes.
+
     Returns:
-        bool: True if all boxes can be opened, False otherwise
+        bool: True if all boxes can be opened, else False
+
+    Example:
+        >>> boxes = [[1], [2], [3], [4], []]
+        >>> canUnlockAll(boxes)
+        True
+        >>> boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+        >>> canUnlockAll(boxes)
+        True
+        >>> boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+        >>> canUnlockAll(boxes)
+        False
     """
-
-    def dfs(box, unlocked):
-        for key in boxes[box]:
-            if key < len(boxes) and key not in unlocked:
-                unlocked.add(key)
-                dfs(key, unlocked)
-
-    if not boxes:
+    n = len(boxes)
+    if n == 0:
         return False
 
-    unlocked = {0}
-    dfs(0, unlocked)
-    return len(unlocked) == len(boxes)
+    unlocked = set([0])  # First box is always unlocked
+    keys = set(boxes[0])  # Keys from the first box
+
+    while keys:
+        new_key = keys.pop()
+
+        # Check if the key opens a valid, new box
+        if (isinstance(new_key, int) and 0 <= new_key < n and
+                new_key not in unlocked):
+            unlocked.add(new_key)
+            # Add new keys from the newly opened box
+            for key in boxes[new_key]:
+                if isinstance(key, int) and key >= 0:
+                    keys.add(key)
+
+    return len(unlocked) == n
