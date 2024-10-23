37d8a1e8adc94459912911533167fc61a86f74b5
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-10-23 13:26:54 +0100

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,43 @@
 #!/usr/bin/python3
 """Log parsing"""
 import sys
-import re
 from typing import Dict
 
+if __name__ == "__main__":
+    def print_stats(status_codes: Dict[str, int], file_size: int):
+        """ function that reads stdin line by line and computes metrics
+        Args:
+            status_codes (dict): dictionary with status codes
+            file_size (int): file size
+        Returns:
+            None
+            """
+        print("File size: {}".format(file_size))
+        for key, value in sorted(status_codes.items()):
+            print("{}: {}".format(key, value))
 
-def print_stats(status_codes: Dict[str, int], file_size: int):
-    """ function that reads stdin line by line and computes metrics
-    Args:
-        status_codes (dict): dictionary with status codes
-        file_size (int): file size
-    Returns:
-        None
-        """
-    print("File size: {}".format(file_size))
-    for key, value in sorted(status_codes.items()):
-        print("{}: {}".format(key, value))
+    total_file_size = 0
+    counter = 0
+    code = 0
+    status_codes = {
+            '200': 0, '301': 0, '400': 0, '401': 0,
+            '403': 0, '404': 0, '405': 0, '500': 0
+    }
+    try:
+        for line in sys.stdin:
+            s_line = line.split()
+            rs_line = s_line[::-1]
+            if len(rs_line) > 2:
+                counter += 1
 
+                if counter <= 10:
+                    total_file_size += int(rs_line[0])
+                    code = rs_line[1]
 
-total_file_size = 0
-counter = 0
-code = 0
-status_codes = {
-        '200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0
-}
-try:
-    for line in sys.stdin:
-        s_line = line.split()
-        rs_line = s_line[::-1]
-        if len(rs_line) > 2:
-            counter += 1
-
-            if counter <= 10:
-                total_file_size += int(rs_line[0])
-                code = rs_line[1]
-
-                if code in status_codes.keys():
-                    status_codes[code] += 1
-        if counter % 10 == 0:
-            print_stats(status_codes, total_file_size)
-            counter = 0
-finally:
-    print_stats(status_codes, total_file_size)
+                    if code in status_codes.keys():
+                        status_codes[code] += 1
+            if counter % 10 == 0:
+                print_stats(status_codes, total_file_size)
+                counter = 0
+    finally:
+        print_stats(status_codes, total_file_size)
