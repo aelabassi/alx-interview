751b68b1b237575c077fe5f8273cb1f8a6d5dcb7
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-11-12 16:57:25 +0100

    star wars characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+const options = {
+  uri: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  json: true
+};
+const res = request(options, (err, res, body) => {
+  if (err) { return console.log(err); }
+  const actors = body.characters;
+  extractInOrder(actors, 0);
+});
+
+const extractInOrder = (actors, key) => {
+  if (actors.length === key) {
+    return;
+  }
+  request(actors[key], (err, res, body) => {
+    if (err) { return console.log(err); }
+    console.log(JSON.parse(body).name);
+    extractInOrder(actors, key + 1);
+  });
+};
---
+++ 0x06-starwars_api/types.d.ts
@@ -0,0 +1,5 @@
+type Options = {
+    uri: string,
+    json: boolean,
+}
+
